8a85c4b48f912be7d9d3312cae893438d29df5cb
ltman <li@listree.com>
2021-03-15 14:34:54 -0700

    Update AlertProducer.java

--- src/main/java/agents/AlertProducer.java
+++ src/main/java/agents/AlertProducer.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 public class AlertProducer {
    
@@ -18,28 +19,33 @@ public class AlertProducer {
       
       //Assign topicName to string variable
       String topic = args[0].toString();
-
-      // create instance for properties to access producer configs
-      Properties props = new Properties();
-      props.put("bootstrap.servers", "localhost:9092");       //Assign localhost id
-      props.put("acks", "all");      //Set acknowledgements for producer requests.
-      props.put("retries", 0);       //If the request fails, the producer can automatically retry,
-      props.put("batch.size", 16384);       //Specify buffer size in config
-      props.put("linger.ms", 1);       //Reduce the no of requests less than 0
-      props.put("buffer.memory", 33554432);  //total memory available to the producer for buffering.
-      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(props);
+      Properties props = getProperties();
+      Producer<String, String> producer = new KafkaProducer<String, String>(props);
 
       int i = 0;
       while(i < 3600) { // keep sending message for an hour
-         System.out.println("Sending " + i++ + "th Message");
          String key =  Integer.toString(i  % 10);
          String value = Integer.toString(new Random().nextInt(10));
-         producer.send(new ProducerRecord<String, String>(topic, key, value));
+         System.out.println(String.format("Sending message=%d, key=%d, value=%d", i, key, value));
+         producer.send(new ProducerRecord(topic, key, value));
          Thread.sleep(1000); // every 1 second
+         i++;
       }
 
       producer.close();
+   }
+
+   // create instance for properties to access producer configs
+   private static Properties getProperties() {
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092");       // Assign localhost:port
+      props.put("acks", "all");      // Set acknowledgements for producer requests.
+      props.put("retries", 0);       // If the request fails, the producer can automatically retry,
+      props.put("batch.size", 16384);       // Specify buffer size in config
+      props.put("linger.ms", 1);       // Reduce the no of requests less than 0
+      props.put("buffer.memory", 33554432);  // Total memory available to the producer for buffering.
+      props.put("key.serializer", StringSerializer.class.getCanonicalName());
+      props.put("value.serializer", StringSerializer.class.getCanonicalName());
+      return props;
    }
 }
